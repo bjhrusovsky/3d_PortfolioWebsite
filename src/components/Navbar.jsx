c8de5c3edf418742dab6fa9ab345e604db0dcf2b
Romanpuss <bryce_hrusovsky@yahoo.com>
2023-07-18 14:43:14 -0700

    adding css to main website

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -8,8 +8,9 @@ import { logo, menu, close } from '../assets';
 const Navbar = () => {
   const [ active, setActive] = useState("");
   return (
-    <nav className={`${styles.paddindX} w-full flex items-center py-5 fixed top-0 z-20 bg-primary`}>
-      <div className="w-full flex justify-between items-center max-w-7x1 mx-auto">
+    <nav 
+    className={`${styles.paddindX} w-full flex items-center py-5 fixed top-0 z-20 bg-primary`}>
+      <div className="w-full flex justify-between items-center max-w-7xl mx-auto">
         <Link 
         to="/" 
         className="flex items-center gap-2" 
@@ -20,6 +21,7 @@ const Navbar = () => {
             <img src={logo} alt="logo" className="w-9 h-9 object-contain" />
             <p className="text-white text-[18px] font-bold cursor-pointer">Bryce <span className="sm:block hidden">| JavaScript Mastery</span></p>
         </Link>
+        <p className="text-red-500">Big Mom</p>
 
       </div>
     </nav>
